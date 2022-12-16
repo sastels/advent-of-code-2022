4db02504eafdee976dfda2143704991b9e2ab3c2
Stephen Astels <sastels@gmail.com>
2022-12-16 16:46:35 -0500

    fix tests

--- tests/intervals.rs
+++ tests/intervals.rs
@@ -3,8 +3,8 @@ use advent_2022::intervals::merge_intervals;
 #[test]
 fn test_merge_intervals_disjoint() {
     assert_eq!(
-        merge_intervals(vec![(1, 2), (6, 10), (3, 4)]),
-        vec![(1, 2), (3, 4), (6, 10)]
+        merge_intervals(vec![(1, 2), (6, 10), (4, 4)]),
+        vec![(1, 2), (4, 4), (6, 10)]
     );
 }
 
@@ -16,8 +16,8 @@ fn test_merge_intervals_overlap() {
 #[test]
 fn test_merge_intervals_general() {
     assert_eq!(
-        merge_intervals(vec![(1, 5), (2, 3), (3, 7), (8, 10), (9, 11)]),
-        vec![(1, 7), (8, 11)]
+        merge_intervals(vec![(1, 5), (2, 3), (3, 7), (9, 11), (10, 112)]),
+        vec![(1, 7), (9, 112)]
     );
 }
 
