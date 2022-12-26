06d2a3bc7779bf481efdc27f2d442478f94d27f8
Stephen Astels <sastels@gmail.com>
2022-12-26 17:42:07 -0500

    part 2

--- tests/day18.rs
+++ tests/day18.rs
@@ -1,7 +1,14 @@
-use advent_2022::day18::{solve_a, solve_b};
+use advent_2022::day18::{find_interior, solve_a, solve_b};
 use advent_2022::utils::read_lines;
 
 const TEST_FILE: &str = "./test_data/day18.txt";
+
+#[test]
+fn test_find_interior() {
+    let data = read_lines(TEST_FILE);
+    let cubes = advent_2022::day18::parse_input(&data);
+    assert_eq!(find_interior(&cubes), vec![(2, 2, 5)]);
+}
 
 #[test]
 fn test_solve_a() {
@@ -13,5 +20,5 @@ fn test_solve_a() {
 #[test]
 fn test_solve_b() {
     let data = read_lines(TEST_FILE);
-    assert_eq!(solve_b(&data), 0);
+    assert_eq!(solve_b(&data), 58);
 }
